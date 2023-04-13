5f62b8dea58f90c891e8b9c0b48b31ca79f144d1
manueldembo <madembo18@gmail.com>
2023-04-13 06:03:11 +0100

    feat: useRoute

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Product details for {{ id }}</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ad eaque nostrum necessitatibus impedit voluptates distinctio dolor, rerum laborum qui?</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
