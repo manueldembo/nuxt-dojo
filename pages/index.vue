088b9699b1ca72a7d6a0a9a4b243e0a6a1126e7a
manueldembo <madembo18@gmail.com>
2023-04-13 05:50:24 +0100

    feat: pages structure created

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quasi maiores tempora illum ullam repellat laboriosam voluptatem rem delectus voluptatum. Temporibus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quasi maiores tempora illum ullam repellat laboriosam voluptatem rem delectus voluptatum. Temporibus?
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
